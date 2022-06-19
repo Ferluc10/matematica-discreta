a36dcc738aec85b87e88620e15c470aa5712e76d
Ferluc10 <107676958+Ferluc10@users.noreply.github.com>
2022-06-19 11:59:47 +0200

    Update Entrega.java

--- Entrega.java
+++ Entrega.java
@@ -659,7 +659,15 @@ class Entrega {
      * Retornau l'invers sempre entre 1 i `n-1`, en cas que no existeixi retornau -1
      */
     static int exercici3(int a, int n) {
-      return -1; // TO DO
+        if(exercici1(a,n)!=1){
+            return -1;
+        }
+        for(int i=0;i<n;i++){
+            if(a*i==(n+1)){
+                return i;
+            }
+        }
+        return -1;//DONE
     }
 
     /*
